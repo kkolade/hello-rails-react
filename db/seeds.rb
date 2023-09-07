da9b867f3875510a69fa45870fe89a9d9ae075c3
Kola Kolade <kolakolade@gmail.com>
2023-09-08 00:13:31 +0100

    Generate Messages model, migrate db, seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+unique_greeting = [
+  "Hello, world!",
+  "Greetings from Rails!",
+  "Welcome to my app!",
+  "Have a great day!",
+  "Good to see you!"
+]
+
+5.times do |i|
+  Message.create(greeting: unique_greeting[i])
+end
